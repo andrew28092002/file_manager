99de09956b75541c815e25bd6cc0e4f66c2ece6d
andrew28092002 <andreiastanaev2002@gmail.com>
2023-04-21 09:19:23 +0500

    some small changes in store

--- client/src/stores/file.ts
+++ client/src/stores/file.ts
@@ -1,11 +1,29 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
 
-export const useFileStore = defineStore('file', () => {
-  const leftPath = ref('c')
-  const rightPath = ref('c')
+export const useFileStore = defineStore("file", () => {
+  const leftPath = ref("c");
+  const rightPath = ref("c");
+  const choosenFile = ref("c");
+
+  const chooseFile = (path: string) => {
+    choosenFile.value = path;
+  };
+
+  const changeLeftPath = (path: string) => {
+    leftPath.value = path;
+  };
+
+  const changeRightPath = (path: string) => {
+    leftPath.value = path;
+  };
 
   return {
-    leftPath, rightPath
-  }
-})
+    leftPath,
+    rightPath,
+    choosenFile,
+    chooseFile,
+    changeLeftPath,
+    changeRightPath,
+  };
+});
