b94358bb6e7777ddb7719ad3c88572f9a05791cd
andrew28092002 <andreiastanaev2002@gmail.com>
2023-04-21 11:52:20 +0500

    add watchers

--- client/src/stores/file.ts
+++ client/src/stores/file.ts
@@ -1,10 +1,41 @@
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 import { defineStore } from "pinia";
 
 export const useFileStore = defineStore("fileStore", () => {
   const leftPath = ref("c");
   const rightPath = ref("c");
   const choosenFile = ref("c");
+  const leftFiles = ref([]);
+  const rightFiles = ref([]);
+
+  const watchLeft = watch(
+    () => leftPath,
+    async () => {
+      const files = await getFiles(leftPath.value);
+      leftFiles.value = files;
+    }
+  );
+
+  const watchRight = watch(
+    () => rightFiles,
+    async () => {
+      const files = await getFiles(rightPath.value);
+      leftFiles.value = files;
+    }
+  );
+
+  const getFiles = async (path: string) => {
+    const response = await fetch("", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(path),
+    });
+
+    const files = await response.json();
+    return files;
+  };
 
   const chooseFile = (path: string) => {
     choosenFile.value = path;
@@ -25,5 +56,7 @@ export const useFileStore = defineStore("fileStore", () => {
     chooseFile,
     changeLeftPath,
     changeRightPath,
+    watchLeft,
+    watchRight,
   };
 });
