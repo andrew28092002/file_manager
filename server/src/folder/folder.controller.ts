52558fc1afca42b14a7bceaea89511b9ea65b427
Степанов Алексей <bssclient@gmail.com>
2023-05-02 11:40:35 +0500

    добавил замечания

--- server/src/folder/folder.controller.ts
+++ server/src/folder/folder.controller.ts
@@ -11,21 +11,25 @@ export class FolderController {
     return this.folderService.createFolder(path);
   }
 
+   /** по HTPP это скорее GET */
   @Post('/get')
   getFolder(@Body('path') path: string) {
     return this.folderService.getFolder(path);
   }
 
+  /** по HTPP это скорее PUT */
   @Post('/copy')
   copyFolder(@Body() { pathFrom, pathTo }: PathFromToDto) {
     return this.folderService.copyFolder(pathFrom, pathTo);
   }
 
+   /** по HTPP это скорее PUT */
   @Post('/move')
   moveFolder(@Body() { pathFrom, pathTo }: PathFromToDto) {
     return this.folderService.moveFolder(pathFrom, pathTo);
   }
 
+  /** по HTPP это скорее Delete */
   @Post('/delete')
   deleteFolder(@Body('path') path: string) {
     return this.folderService.deleteFolder(path);
